225c6331b48870e51d3e05cb70996b5c7b68b5e6
utkarsh3042 <u.saraswat30@gmail.com>
2023-02-17 16:30:02 +0530

    simple program to get the largest & smallest int in an array using linear search

---
+++ Linearsearch.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+//simple program to get the largest & smallest int in an array using linear search
+
+int largest(int arr[],int n);
+int smallest(int arr[], int n);
+
+void main()
+{
+    int arr[100],n,i,large,small;
+    printf("enter no. of elements in array:\n");
+    scanf("%d",&n);
+    printf("enter values in array\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);    
+    }
+    large = largest(arr,n);
+    small = smallest(arr,n);
+    printf("Largest = %d\n",large);
+    printf("Smallest = %d\n",small);
+
+    
+}
+
+int largest(int arr[],int n){
+    int large,i;
+    large = arr[0];
+    for(i=0;i<n;i++){
+        if(large<arr[i]){
+            large=arr[i];
+        }
+    }
+    return large;
+}
+
+int smallest(int arr[],int n){
+    int large,i;
+    large = arr[0];
+    for(i=0;i<n;i++){
+        if(large>arr[i]){
+            large=arr[i];
+        }
+    }
+    return large;
+}
